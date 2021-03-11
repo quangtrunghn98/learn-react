5ab20c5a0d55c3388bd57053f955cce993db77ef
Ngo Quang Trung <quangtrunghn98@gmail.com>
2021-03-11 13:30:41 +0700

    add getAll for productApi

--- src/features/Product/index.js
+++ src/features/Product/index.js
@@ -1,14 +1,22 @@
 import { Box } from '@material-ui/core';
-import React from 'react';
+import producApi from 'api/productApi';
+import React, { useEffect } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router';
 import ListPage from './pages/ListPage';
 
 ProductFeature.propTypes = {
-  
+
 };
 
 function ProductFeature(props) {
   const match = useRouteMatch();
+
+  useEffect(() => {
+    (async () => {
+      const response = await producApi.getAll({ _page: 1, _limit: 10 });
+      console.log({ response })
+    })();
+  }, [])
 
   return (
     <Box pt={4}>
